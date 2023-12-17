5449f04c9cb76b29f220bce3c1507f788412cb88
FelipeCatalan12 <felipeign767@gmail.com>
2023-12-17 20:21:31 -0300

    Cambios settings

--- intranetTrabajadores2/settings.py
+++ intranetTrabajadores2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'gestorUsers',
+    'panelApp'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'intranetTrabajadores2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,8 +127,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+"""Pagina post login"""
+LOGIN_REDIRECT_URL = "/vistaAdmin/"
+
+"""Pagina post logout"""
+LOGOUT_REDIRECT_URL = "/accounts/login/"
+
+"""Pagina Login"""
+LOGIN_URL = "/accounts/login/"
+
+"""Pagina Logout"""
+LOGOUT_URL = "/accounts/logout/"
